afb39e60df804723752cde2bf1ec0b76b996043a
mateorumac <mateo.rumac@gmail.com>
2023-08-31 18:06:34 +0200

    login error message

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -130,9 +130,7 @@ export default {
           this.$router.push("/");
           console.log("Uspješna prijava");
         } else {
-          
-          console.error(e.message);
-          res.status(403).json({ error: e.message });
+          console.log("error", e.message);
         }
       } catch (error) {
           const errorMessage = error && error.message ? error.message : "Invalid email or password";
